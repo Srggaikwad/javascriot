9f682b8728171ea1cb12d7378a60a2153416ffc5
Srggaikwad <surajgaikwad1356@gmail.com>
2023-12-26 16:57:18 +0530

    javascript
    javascript

---
+++ helloworld.js
@@ -0,0 +1,28 @@
+console.log("hello js,i am learning javascript");
+
+var myname;
+
+console.log(myname);
+
+myname="suraj gaikwad";
+
+console.log(myname);
+console.log("my full name is:",myname);
+
+console.log("i love sonu");
+
+myname="suraj";
+
+console.log("firstname:",myname);
+
+var n1=20;
+var n2=30;
+
+console.log("the value of n1 and n2 before swap is","n1=",n1  ,"n2=",n2)
+
+var temp;
+temp=n1;
+n1=n2;
+n2=temp;
+
+console.log("the value of n1 and n2 after swap is","n1=",n1  ,"n2=",n2)
