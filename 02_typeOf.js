b1275e2f370000748c969f619c21480d7a65e42d
Srggaikwad <surajgaikwad1356@gmail.com>
2023-12-27 21:17:36 +0530

    javascript
    javascript

---
+++ 02_typeOf.js
@@ -0,0 +1,45 @@
+console.log("****************first stpe***************")
+
+var bankname;
+
+console.log("the value of bank without initialization is:",bankname,",type is:",typeof(bankname));
+
+bankname="SBI Bank";
+
+console.log("the value of bank after initialization is:",bankname,",type is:",typeof(bankname));
+
+console.log("***************secound stpe****************")
+
+
+var marks="90%";
+
+console.log("marks is",marks,"and is datatype:",typeof(marks));
+
+
+console.log("***************third step*********************")
+
+console.log("before initilization");
+
+var isworking;
+
+console.log("working status:" ,isworking ,"type:",typeof(isworking))
+
+console.log("after initialization")
+
+isworking=true;
+console.log("the working status is:",isworking,"type is:",typeof(isworking));
+
+console.log("**************step 4")
+
+console.log("before updating the value is ");
+
+
+totalCount="Hundred and seven";
+
+console.log("totolcound is:",totalCount,"type is:",typeof(totalCount));
+
+totalCount=107;
+
+console.log("after update ");
+
+console.log("totolcount is:",totalCount,"type is:",typeof(totalCount));
