4ddd353380e32b23ad241adb124653399739e62b
Srggaikwad <surajgaikwad1356@gmail.com>
2023-12-27 21:36:07 +0530

    javascript
    javascript

---
+++ 02_variableBasicsAssig.js
@@ -0,0 +1,22 @@
+var name="suraj";
+
+console.log("name:",name);
+console.log("data type is:",typeof(name));
+
+console.log("******************************")
+
+var age=22;
+console.log("age:",age);
+console.log("data type is:",typeof(age));
+
+console.log("********************************")
+
+var martial=false;
+console.log("martial status:",martial);
+console.log("data type is:",typeof(martial));
+
+console.log("****************************")
+
+console.log("the difference between null and undifined is:")
+
+console.log("the undefined means you declare the variable but not initialize it and null means nothing empty ")
