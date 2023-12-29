027a53e79db560f7b763b0bfcf9f0b17effeb6e5
Srggaikwad <surajgaikwad1356@gmail.com>
2023-12-29 10:42:05 +0530

    bankdetails
    bankdetails

---
+++ 03_funAssign02.js
@@ -0,0 +1,14 @@
+
+
+function bankdetails(bankname,accountnum,location,pincode){
+
+    console.log("bankname:",bankname,"accountnum:",accountnum,"location:",location,"pincode:",pincode)
+
+
+}
+
+bankdetails("sbi",12456,"sangola",413307);
+bankdetails("boi",65464694541,"pune",413517);
+
+bankdetails("union bank",65416464654,"solapur",8545467);
+bankdetails("axis",3214654,"mumbai",6544);
